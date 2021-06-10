2ca1a2a370f367e5b65915c0295c8b539e332cce
Maxy <maxy.dp@abv.bg>
2021-06-10 19:40:11 +0300

    added component

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const app = Vue.createApp({
+  template: `
+    <div class="container">
+        <animated-list :randomLists="arrOfStrings"></animated-list>
+    </div>
+    `,
   data() {
     return {
-      name: "Ivan",
+      arrOfStrings: ["Banana", "Apple", "V8", "Motorcycle", "Kinesiotape"],
     };
+  },
+});
+
+app.component("animated-list", {
+  template: `
+    <ul>
+        <li v-for="randomLi in randomLists" :key="randomLi">{{ randomLi }}</li>
+    </ul>
+    `,
+  props: {
+    randomLists: Object,
   },
 });
 
