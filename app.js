6c571a06fbea2b82e037f78bfd4d9b3d24568e41
Maxy <maxy.dp@abv.bg>
2021-06-10 20:24:38 +0300

    updated js & css

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const app = Vue.createApp({
   template: `
     <div class="container">
-        <animated-list :randomLists="arrOfStrings"></animated-list>
+        <animated-list></animated-list>
+    </div>
+    `,
+});
+
+// Components
+app.component("animated-list", {
+  template: `
+    <transition-group tag="ul" name="animated-list">
+        <li v-for="string in arrOfStrings" :key="string" @click="removeWord(string)">{{ string }}</li>
+    </transition-group>
+    <div>
+        <input type="text" ref="stringInput" placeholder="Enter something...">
+        <button @click="addWord">Add Me</button>
     </div>
     `,
   data() {
@@ -9,17 +22,13 @@ const app = Vue.createApp({
       arrOfStrings: ["Banana", "Apple", "V8", "Motorcycle", "Kinesiotape"],
     };
   },
-});
-
-app.component("animated-list", {
-  template: `
-    <ul>
-        <li v-for="randomLi in randomLists" :key="randomLi">{{ randomLi }}</li>
-    </ul>
-    `,
-  props: {
-    randomLists: Object,
+  methods: {
+    addWord() {
+      this.arrOfStrings.unshift(this.$refs.stringInput.value);
+    },
+    removeWord(string) {
+      this.arrOfStrings = this.arrOfStrings.filter((val) => val !== string);
+    },
   },
 });
-
 app.mount("#app");
